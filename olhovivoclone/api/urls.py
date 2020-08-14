ba66c3683b9050f014764e8164fc330edb206821
Breno Nahuz <35977737+bnahuz@users.noreply.github.com>
2020-08-13 22:43:36 -0300

    Implementado PUT,DELETE

--- olhovivoclone/api/urls.py
+++ olhovivoclone/api/urls.py
@@ -3,8 +3,16 @@ from . import views
 
 urlpatterns = [
     url(r'^paradas/$', views.ParadaList.as_view(), name='music-list'),
+    url(r'^parada/(?P<pk>[0-9]+)/$', views.ParadaDetail.as_view(), name='parada-detail'),
+
     url(r'^linhas/$', views.LinhaList.as_view(), name='music-list'),
+    url(r'^linha/(?P<pk>[0-9]+)/$', views.LinhaDetail.as_view(), name='linha-detail'),
+
     url(r'^veiculos/$', views.VeiculoList.as_view(), name='music-list'),
+    url(r'^veiculo/(?P<pk>[0-9]+)/$', views.VeiculoDetail.as_view(), name='veiculo-detail'),
+    
     url(r'^posicoes/$', views.PosicaoVeiculoList.as_view(), name='music-list'),
+    url(r'^posicao/(?P<pk>[0-9]+)/$', views.PosicaoVeiculoDetail.as_view(), name='posicao-detail'),
+
 
 ]
--- olhovivoclone/api/views.py
+++ olhovivoclone/api/views.py
@@ -7,7 +7,28 @@ class ParadaList(generics.ListCreateAPIView):
     queryset = Parada.objects.all()
     serializer_class = ParadaSerializer
 
+
+'''class ParadaList(generics.ListCreateAPIView):
+    serializer_class = ParadaSerializer
+
+    def get_queryset(self):
+        queryset = Parada.objects.all()
+        linha = self.request.query_params.get('linha', None)
+        if linha is not None:
+            queryset = queryset.filter(linhas__parada=linha)
+            
+        return queryset'''    
+
+class ParadaDetail(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Parada.objects.all()
+    serializer_class = ParadaSerializer
 class LinhaList(generics.ListCreateAPIView):
+
+    queryset = Linha.objects.all()
+    serializer_class = LinhaSerializer
+
+class LinhaDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Linha.objects.all()
     serializer_class = LinhaSerializer
@@ -17,7 +38,17 @@ class VeiculoList(generics.ListCreateAPIView):
     queryset = Veiculo.objects.all()
     serializer_class = VeiculoSerializer
 
+class VeiculoDetail(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Veiculo.objects.all()
+    serializer_class = VeiculoSerializer
+
 class PosicaoVeiculoList(generics.ListCreateAPIView):
+
+    queryset = PosicaoVeiculo.objects.all()
+    serializer_class = PosicaoVeiculoSerializer
+
+class PosicaoVeiculoDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = PosicaoVeiculo.objects.all()
     serializer_class = PosicaoVeiculoSerializer
